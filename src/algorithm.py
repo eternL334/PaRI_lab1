b8c3f2244511dcfa76fc4937cc961ca057e2bd8a
eternl <dan_arist04@mail.ru>
2024-03-23 20:56:31 +0300

    add algorithm

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,3 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
+
+def get_mask(img, lower, upper):
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    mask = cv.inRange(hsv, lower, upper)
+    return mask
+
+def get_components(img, size=0):
+    n, res, stats, centroids = cv.connectedComponentsWithStats(img)
+    size_stat = stats[:, -1]
+    small, = np.where(size_stat < size)
+    img[np.isin(res, small)] = 0
+    return n - len(small), img 
+    
