319b0bd0dc706effa4a391076ce5fa24a96d9835
eternl <dan_arist04@mail.ru>
2024-03-24 20:49:36 +0300

    add docker

--- src/server.py
+++ src/server.py
@@ -66,5 +66,4 @@ def predict():
     return render_template("predict.html", n=n)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # serve(app, host='0.0.0.0', port='5000')
+    serve(app, host='0.0.0.0', port='5000')
